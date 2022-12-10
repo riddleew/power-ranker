3a2e81792b86754e19ac8928c7424c4cbd290ba3
Eric <riddle.e.w@gmail.com>
2022-12-10 13:52:21 -0500

    Query events per tourney, add too collection

--- main.py
+++ main.py
@@ -8,16 +8,35 @@ import queries
 from datamodels import *
 
 
-def get_token():
-  """Retrieves oauth token from a text file."""
+def get_tokens():
+  """Retrieves oauth tokens from a text file."""
+  
+  tokens = []
   with open('token.txt', 'r') as file:
-    return file.readline()
+    for token in file:
+      tokens.append(token.strip())
+
+  return tokens
+
+
+def reset_client():
+  """Before reaching request limit, inject a fresh auth token to reset request count."""
+  
+  global current_token_index
+  global request_count 
+  request_count = 0
+
+  client.inject_token('Bearer ' + auth_tokens[current_token_index])
+  if current_token_index == len(auth_tokens) - 1:
+    current_token_index = 0
+  else:
+    current_token_index += 1
 
 
 def collect_user_ids_from_file():
   """Reads through a text file and compiles a dictionary of user_id -> player_name."""
 
-  with open('user-ids-half.txt', 'r') as file:
+  with open('user-ids.txt', 'r') as file:
     delimiter = '---'
     for line in file:
       if line.startswith('#'):
@@ -40,18 +59,8 @@ def set_tournaments():
   for user_id, player_name in user_dict.items():
     print("Processing " + player_name + "'s tournaments...")
     query_variables = {"userId": user_id}
-    try :
-      result = client.execute(queries.get_tournies_by_user, query_variables)
-    except URLError:
-      try_attempt_ctr = 1
-      total_attempts = 3
-      while try_attempt_ctr <= total_attempts:
-        try:
-          result = client.execute
-          break
-        except URLError:
-          try_attempt_ctr
-    
+
+    result = execute_query(queries.get_tournies_by_user, query_variables)
     res_data = json.loads(result)
     if 'errors' in res_data:
         print('Error:')
@@ -77,6 +86,26 @@ def set_tournaments():
   return tourney_dict
 
 
+def execute_query(query, variables):
+  """Executes GraphQL queries. Implements a retry system if a bad connection occurs."""
+  
+  try_attempt_ctr = 1
+  total_attempts = 3
+  global request_count
+
+  while try_attempt_ctr <= total_attempts:
+    if request_count == request_threshold:
+      reset_client()
+    try:
+      request_count += 1
+      result = client.execute(query, variables)
+      return result
+    except URLError:
+      try_attempt_ctr += 1
+
+  raise URLError(f'Max number of attempts ({total_attempts}) exceeded.')
+
+
 def write_tourney_names_to_files(tournies):
   """Writes tourney names/slugs with dates to text files.
   Allows for a simple overview summary of tournaments.
@@ -91,13 +120,18 @@ def write_tourney_names_to_files(tournies):
       print(f'{i}: {tourney.slug}')
       i = i + 1
 
+
 def set_events(tournies):
-  events = []
+  """Queries events per tournaments. Attempts to filter out non-Singles events.
+  Adds results to collection.
+  """
   global client
+  filter_names = {'squad strike', 'crew battle', 'redemption', 'ladder', 'doubles', 'amateur'}
   
   for tourney in tournies:
+    print(f'\n{tourney.name}')
     query_variables = {"slug": tourney.slug}
-    result = client.execute(queries.get_event_by_tournament, query_variables)
+    result = execute_query(queries.get_event_by_tournament, query_variables)
     res_data = json.loads(result)
     if 'errors' in res_data:
         print('Error:')
@@ -105,17 +139,38 @@ def set_events(tournies):
 
     for event_json in res_data['data']['tournament']['events']:
       event = Event(event_json)
+
+      # Filter out events that are most likely not Singles events
       if event.is_teams_event:
         continue
+      is_not_singles = 1 in [name in event.name.lower() for name in filter_names]
+      if is_not_singles:
+        continue
       
-  return events
+      print(f'---{event.name}')
+      add_element_to_tourney_to_events(tourney.slug, event)
 
-auth_token = get_token()
+
+def add_element_to_tourney_to_events(tourney_slug, event):
+  """Updates tourney_to_events mapping."""
+  
+  if tourney_slug not in tourney_to_events:
+    tourney_to_events[tourney_slug] = []
+  
+  tourney_to_events[tourney_slug].append(event)
+
+
+auth_tokens = get_tokens()
+current_token_index = 0
 api_version = 'alpha'
+request_count = 0
 client = GraphQLClient('https://api.smash.gg/gql/' + api_version)
-client.inject_token('Bearer ' + auth_token)
+reset_client()
 ultimate_id = '1386'
 user_dict = dict()
+tourney_to_events = dict()
+
+request_threshold = 79
 
 collect_user_ids_from_file()
 
@@ -125,3 +180,12 @@ tournies = sorted(set_tournaments().values(), key=lambda tourney: tourney.start_
 
 write_tourney_names_to_files(tournies)
 set_events(tournies)
+with open('tourney_events.txt', 'w') as file:
+  for tourney_slug, events in tourney_to_events.items():
+    file.write(f'{tourney_slug}\n')
+    for event in events:
+      file.write(f'---{event.name}\n')
+
+debug = -1
+
+# Add all_events_removed_from_tourney idea
